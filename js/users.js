ff9aa0be468291d5fc3ca0f1eb640bb9fc4c93b3
hexuemei <hexuemei@hongte.info>
2019-05-21 16:17:21 +0800

    up

---
+++ js/users.js
@@ -0,0 +1,450 @@
+var members = [
+    {
+        "fileName": "1.jpg",
+        "memberName": "夏庆华",
+        "fullName": "夏庆华"
+    },
+    {
+        "fileName": "2.jpg",
+        "memberName": "李华杰",
+        "fullName": "李华杰"
+    },
+    {
+        "fileName": "3.jpg",
+        "memberName": "路人丙",
+        "fullName": "林月榕"
+    },
+    {
+        "fileName": "4.jpg",
+        "memberName": "路人丁",
+        "fullName": "林锦川"
+    },
+    {
+        "fileName": "5.jpg",
+        "memberName": "蔡海亮",
+        "fullName": "蔡海亮"
+    }
+    ,
+    {
+        "fileName": "6.jpg",
+        "memberName": "邱文武",
+        "fullName": "邱文武"
+    }
+    ,
+    {
+        "fileName": "7.jpg",
+        "memberName": "苏焕城",
+        "fullName": "苏焕城"
+    }
+    ,
+    {
+        "fileName": "8.jpg",
+        "memberName": "姚杰鸿",
+        "fullName": "姚杰鸿"
+    }
+    ,
+    {
+        "fileName": "9.jpg",
+        "memberName": "钱淑霞",
+        "fullName": "钱淑霞"
+    }
+    ,
+    {
+        "fileName": "10.jpg",
+        "memberName": "19年大吉大利-陈丽萍",
+        "fullName": "19年大吉大利-陈丽萍"
+    }
+    ,
+    {
+        "fileName": "11.jpg",
+        "memberName": "必中大奖-刘美欣",
+        "fullName": "必中大奖-刘美欣"
+    }
+    ,
+    {
+        "fileName": "12.jpg",
+        "memberName": "不文静的钟文静-钟文静",
+        "fullName": "不文静的钟文静-钟文静"
+    }
+    ,
+    {
+        "fileName": "13.jpg",
+        "memberName": "才貌双全-曾爱茵",
+        "fullName": "才貌双全-曾爱茵"
+    }
+    ,
+    {
+        "fileName": "14.jpg",
+        "memberName": "操碎心的猫奴老母亲-孙智慧",
+        "fullName": "操碎心的猫奴老母亲-孙智慧"
+    }
+    ,
+    {
+        "fileName": "15.jpg",
+        "memberName": "大吉大利-黄小利",
+        "fullName": "大吉大利-黄小利"
+    }
+    ,
+    {
+        "fileName": "16.jpg",
+        "memberName": "大奖得主-罗小荷",
+        "fullName": "大奖得主-罗小荷"
+    }
+    ,
+    {
+        "fileName": "17.jpg",
+        "memberName": "大奖快到我的碗里来-陈彩萍",
+        "fullName": "大奖快到我的碗里来-陈彩萍"
+    }
+    ,
+    {
+        "fileName": "18.jpg",
+        "memberName": "大礼一锋-吕彦锋",
+        "fullName": "大礼一锋-吕彦锋"
+    }
+    ,
+    {
+        "fileName": "19.JPG",
+        "memberName": "戴红帽子的小仙女-王宏新",
+        "fullName": "戴红帽子的小仙女-王宏新"
+    }
+    ,
+    {
+        "fileName": "20.jpg",
+        "memberName": "抖音刷起来-韩玲",
+        "fullName": "抖音刷起来-韩玲"
+    }
+    ,
+    {
+        "fileName": "21.jpg",
+        "memberName": "恶作剧-黄晓君",
+        "fullName": "恶作剧-黄晓君"
+    }
+    ,
+    {
+        "fileName": "22.jpg",
+        "memberName": "飞天小猪-邱娟娟",
+        "fullName": "飞天小猪-邱娟娟"
+    }
+    ,
+    {
+        "fileName": "23.jpg",
+        "memberName": "攻城狮们的政委-邓健梅",
+        "fullName": "攻城狮们的政委-邓健梅"
+    }
+    ,
+    {
+        "fileName": "24.jpg",
+        "memberName": "很瘦很瘦-徐伟娴",
+        "fullName": "很瘦很瘦-徐伟娴"
+    }
+    ,
+    {
+        "fileName": "25.JPG",
+        "memberName": "红红火火中大奖-梁妙",
+        "fullName": "红红火火中大奖-梁妙"
+    }
+    ,
+    {
+        "fileName": "26.jpg",
+        "memberName": "今晚打老虎-石伟",
+        "fullName": "今晚打老虎-石伟"
+    }
+    ,
+    {
+        "fileName": "27.jpg",
+        "memberName": "金猪吉祥-符玉娟",
+        "fullName": "金猪吉祥-符玉娟"
+    }
+    ,
+    {
+        "fileName": "28.jpg",
+        "memberName": "辣妈-饶婷",
+        "fullName": "辣妈-饶婷"
+    }
+    ,
+    {
+        "fileName": "29.jpg",
+        "memberName": "辣妹子-黄回芬",
+        "fullName": "辣妹子-黄回芬"
+    }
+    ,
+    {
+        "fileName": "30.jpg",
+        "memberName": "礼仪美眉-陈诗允",
+        "fullName": "礼仪美眉-陈诗允"
+    }
+    ,
+    {
+        "fileName": "31.jpg",
+        "memberName": "林小牛-林思群",
+        "fullName": "林小牛-林思群"
+    }
+    ,
+    {
+        "fileName": "32.jpg",
+        "memberName": "马云李彦宏王健林的合成-马宏林",
+        "fullName": "马云李彦宏王健林的合成-马宏林"
+    }
+    ,
+    {
+        "fileName": "33.jpg",
+        "memberName": "美丽主持人-刘挡兰",
+        "fullName": "美丽主持人-刘挡兰"
+    }
+    ,
+    {
+        "fileName": "34.JPG",
+        "memberName": "皮肤超好-曾永娟",
+        "fullName": "皮肤超好-曾永娟"
+    }
+    ,
+    {
+        "fileName": "35.jpg",
+        "memberName": "勤劳的豆豆-邓存娣",
+        "fullName": "勤劳的豆豆-邓存娣"
+    }
+    ,
+    {
+        "fileName": "36.jpg",
+        "memberName": "人中锦鲤-陈泽森",
+        "fullName": "人中锦鲤-陈泽森"
+    }
+    ,
+    {
+        "fileName": "37.jpg",
+        "memberName": "三年抱俩三间五房-林伟才",
+        "fullName": "三年抱俩三间五房-林伟才"
+    }
+    ,
+    {
+        "fileName": "38.jpg",
+        "memberName": "文静文静-文巧玲",
+        "fullName": "文静文静-文巧玲"
+    }
+    ,
+    {
+        "fileName": "39.jpg",
+        "memberName": "我要发发发-祁慧珍",
+        "fullName": "我要发发发-祁慧珍"
+    }
+    ,
+    {
+        "fileName": "40.jpg",
+        "memberName": "(钱）途无量-钱锦萍",
+        "fullName": "(钱）途无量-钱锦萍"
+    }
+    ,
+    {
+        "fileName": "41.jpg",
+        "memberName": "犀利人生-麦结文麦结文",
+        "fullName": "犀利人生-麦结文麦结文"
+    }
+    ,
+    {
+        "fileName": "42.jpg",
+        "memberName": "小二-韩晓依",
+        "fullName": "小二-韩晓依"
+    }
+    ,
+    {
+        "fileName": "43.jpg",
+        "memberName": "笑容满满-魏仁娟",
+        "fullName": "笑容满满-魏仁娟"
+    }
+    ,
+    {
+        "fileName": "44.JPG",
+        "memberName": "氧气少女-胡利娟",
+        "fullName": "氧气少女-胡利娟"
+    }
+
+    ,
+    {
+        "fileName": "45.jpg",
+        "memberName": "一夜暴富-黄德成",
+        "fullName": "一夜暴富-黄德成"
+    }
+    ,
+    {
+        "fileName": "46.jpg",
+        "memberName": "中奖中奖-许红金",
+        "fullName": "中奖中奖-许红金"
+    }
+    ,
+    {
+        "fileName": "47.jpg",
+        "memberName": "猪年好运-龙梅",
+        "fullName": "猪年好运-龙梅"
+    }
+    ,
+    {
+        "fileName": "48.jpg",
+        "memberName": "祝我发达-王凯媚",
+        "fullName": "祝我发达-王凯媚"
+    }
+    ,
+    {
+        "fileName": "49.jpg",
+        "memberName": "莫检安",
+        "fullName": "莫检安"
+    }
+    ,
+    {
+        "fileName": "50.jpg",
+        "memberName": "何灿",
+        "fullName": "何灿"
+    }
+    ,
+    {
+        "fileName": "51.jpg",
+        "memberName": "黄辉",
+        "fullName": "黄辉"
+    }
+    ,
+    {
+        "fileName": "52.jpg",
+        "memberName": "蔡强",
+        "fullName": "蔡强"
+    }
+    ,
+    {
+        "fileName": "53.jpg",
+        "memberName": "喻尊龙",
+        "fullName": "喻尊龙"
+    }
+    ,
+    {
+        "fileName": "54.jpg",
+        "memberName": "黎炜灵",
+        "fullName": "黎炜灵"
+    }
+    ,
+    {
+        "fileName": "55.jpg",
+        "memberName": "陈泽圣",
+        "fullName": "陈泽圣"
+    }
+    ,
+    {
+        "fileName": "56.jpg",
+        "memberName": "刘澎城",
+        "fullName": "刘澎城"
+    }
+    ,
+    {
+        "fileName": "57.jpg",
+        "memberName": "符楚",
+        "fullName": "符楚"
+    }
+    ,
+    {
+        "fileName": "58.jpg",
+        "memberName":"唐文攀",
+        "fullName": "唐文攀"
+    }
+    ,
+    {
+        "fileName": "59.jpg",
+        "memberName":"雷浩洁",
+        "fullName": "雷浩洁"
+    }
+    ,
+    {
+        "fileName": "60.jpg",
+        "memberName":"曾坤",
+        "fullName": "曾坤"
+    }
+    ,
+    {
+        "fileName": "61.jpg",
+        "memberName": "肖莹环",
+        "fullName": "肖莹环"
+    }
+    ,
+    {
+        "fileName": "62.jpg",
+        "memberName": "李芳",
+        "fullName": "李芳"
+    }
+    ,
+    {
+        "fileName": "63.jpg",
+        "memberName": "吴确林",
+        "fullName": "吴确林"
+    }
+    ,
+    {
+        "fileName": "64.jpg",
+        "memberName": "曾毓智",
+        "fullName": "曾毓智"
+    }
+    ,
+    {
+        "fileName": "65.jpg",
+        "memberName": "龙雪莲",
+        "fullName": "龙雪莲"
+    }
+    ,
+    {
+        "fileName": "66.jpg",
+        "memberName": "霍金铭",
+        "fullName": "霍金铭"
+    }
+    ,
+    {
+        "fileName": "67.jpg",
+        "memberName": "袁娜",
+        "fullName": "袁娜"
+    }
+    ,
+    {
+        "fileName": "68.jpg",
+        "memberName": "向舜捷",
+        "fullName": "向舜捷"
+    }
+    ,
+    {
+        "fileName": "69.jpg",
+        "memberName": "张嘉惠",
+        "fullName": "张嘉惠"
+    }
+    ,
+    {
+        "fileName": "70.jpg",
+        "memberName": "孙进文",
+        "fullName": "孙进文"
+    }
+    ,
+    {
+        "fileName": "71.jpg",
+        "memberName": "张鹏",
+        "fullName": "张鹏"
+    }
+    ,
+    {
+        "fileName": "72.jpg",
+        "memberName": "雷日聪",
+        "fullName": "雷日聪"
+    }
+    ,
+    {
+        "fileName": "73.jpg",
+        "memberName": "何峰",
+        "fullName": "何峰"
+    }
+    ,
+    {
+        "fileName": "74.jpg",
+        "memberName": "何雪梅",
+        "fullName": "何雪梅"
+    }
+    ,
+    {
+        "fileName": "75.jpg",
+        "memberName": "唐光德",
+        "fullName": "唐光德"
+    }
+    
+    
+];
